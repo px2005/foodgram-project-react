8dd281d909a5b1f8e60dda556c95f0e86ed3b1da
Gorynichev.AA <px2005@yandex.ru>
2022-07-28 10:56:35 +0300

    admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Ingredient, Recipe, Tag
+from users.models import CustomUser, Follow
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'author')
+    list_filter = ('user', 'author')
+    search_fields = ('user', 'author')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Recipe)
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'tags')
+    list_filter = ('name', 'author', 'tags')
+    search_fields = ('name', 'author', 'tags')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Ingredient)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
